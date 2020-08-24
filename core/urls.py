45cc5e4866bb99b74ddf163300d52ca3eaf66aad
tbaror <tbaror@gmail.com>
2020-08-24 12:32:56 +0300

    extend user attrib

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.urls import path, include  # add this
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
@@ -12,3 +14,6 @@ urlpatterns = [
     path("", include("authentication.urls")), # Auth routes - login / register
     path("", include("app.urls"))             # UI Kits Html files
 ]
+
+if getattr(settings, 'DEBUG', False):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
