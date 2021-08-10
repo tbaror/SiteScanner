e1f1c893e29a9c5f64b574bea00fb9539da4ffd6
tbaror <tal_baror@hotmail.com.com>
2021-08-10 09:06:26 +0300

    db progress

--- nmap2db.py
+++ nmap2db.py
@@ -15,34 +15,38 @@ f.close()
 
 nmap_results = xmltodict.parse(xml_content)
 
+class NmapResaultOperations():
+    scan_name = ''
+    jr = ''
 
-def get_scandetails():
-    dt = datetime.datetime.now()
-    scan_date = str(dt.year) + '_' + str(dt.month) + '_' + str(dt.day) + '_' + str(dt.hour) + '_' + str(dt.minute) + '_' + str(dt.second)
-        
-    site_name='"Gefen IL"'
-    scan_name = 'Gefen'+ '_' + scan_date
-    location_name = '"Gefen Dekel Israel,BeerSheba"'
-    lon = "31.263414931238763"
-    lat = "34.81140918899447"
-    site_ip_range1 = '212.143.237.0/24'
-    scan_time_start = nmap_results["nmaprun"]['@start']
-    scan_timestr_start = nmap_results["nmaprun"]['@startstr']
-    scan_args = nmap_results["nmaprun"]['@args']
-    nmap_version = nmap_results["nmaprun"]['@version']
-    scan_time_end = nmap_results["nmaprun"]['runstats']['finished']['@time']
-    scan_timestr_end = nmap_results["nmaprun"]['runstats']['finished']['@timestr']
-    scan_elapsed = nmap_results["nmaprun"]['runstats']['finished']['@elapsed']
-    scan_args = scan_args.replace('"','')
-    scan_args = scan_args.replace('\\','')
-    print(scan_args)
-    argset="--scan_name="+ scan_name + " --site_name="+site_name +  " --location_name="+location_name+ \
-        " --lon="+lon+" --lat="+lat+" --site_ip_range1="+site_ip_range1+" --scan_time_start="+scan_time_start+ \
-        " --scan_timestr_start="+'"'+scan_timestr_start+'"'+" --scan_time_end="+scan_time_end+" --scan_timestr_end="+'"'+scan_timestr_end+'"'+ \
-        " --scan_elapsed="+scan_elapsed+" --scan_args="+'"'+scan_args+'"'+" --nmap_version="+nmap_version
-    print(argset)
-    os.system("python manage.py reportingest "+argset)
-         
+
+    def get_scandetails(self):
+        dt = datetime.datetime.now()
+        scan_date = str(dt.year) + '_' + str(dt.month) + '_' + str(dt.day) + '_' + str(dt.hour) + '_' + str(dt.minute) + '_' + str(dt.second)
+            
+        site_name='"Gefen IL"'
+        self.scan_name = 'Gefen'+ '_' + scan_date
+        location_name = '"Gefen Dekel Israel,BeerSheba"'
+        lon = "31.263414931238763"
+        lat = "34.81140918899447"
+        site_ip_range1 = '212.143.237.0/24'
+        scan_time_start = nmap_results["nmaprun"]['@start']
+        scan_timestr_start = nmap_results["nmaprun"]['@startstr']
+        scan_args = nmap_results["nmaprun"]['@args']
+        nmap_version = nmap_results["nmaprun"]['@version']
+        scan_time_end = nmap_results["nmaprun"]['runstats']['finished']['@time']
+        scan_timestr_end = nmap_results["nmaprun"]['runstats']['finished']['@timestr']
+        scan_elapsed = nmap_results["nmaprun"]['runstats']['finished']['@elapsed']
+        scan_args = scan_args.replace('"','')
+        scan_args = scan_args.replace('\\','')
+        print(scan_args)
+        argset="--scan_name="+ self.scan_name + " --site_name="+site_name +  " --location_name="+location_name+ \
+            " --lon="+lon+" --lat="+lat+" --site_ip_range1="+site_ip_range1+" --scan_time_start="+scan_time_start+ \
+            " --scan_timestr_start="+'"'+scan_timestr_start+'"'+" --scan_time_end="+scan_time_end+" --scan_timestr_end="+'"'+scan_timestr_end+'"'+ \
+            " --scan_elapsed="+scan_elapsed+" --scan_args="+'"'+scan_args+'"'+" --nmap_version="+nmap_version
+        print(argset)
+        os.system("python manage.py sitessestingest "+argset)
+            
    
 
         
@@ -50,15 +54,18 @@ def get_scandetails():
 
 
 
-def get_address(c):
-    addlen = (len(nmap_results["nmaprun"]["host"][c]['address']))
-    print(addlen)
-    for ad in range(addlen):
-        if '@addr' in nmap_results['nmaprun']['host'][c]['address'][ad]:
-            print( nmap_results['nmaprun']['host'][c]['address'][ad]['@addr']) 
-            print(nmap_results['nmaprun']['host'][c]['address'][ad]['@addrtype'])
-            if '@vendor' in nmap_results['nmaprun']['host'][c]['address'][ad]:
-                print(nmap_results['nmaprun']['host'][c]['address'][ad]['@vendor'])
+    def get_address(self,jr):
+        addlen = (len(nmap_results["nmaprun"]["host"][jr]['address']))
+        print(addlen)
+        for ad in range(addlen):
+            if '@addr' in nmap_results['nmaprun']['host'][jr]['address'][ad]:
+                host_ip_name=nmap_results['nmaprun']['host'][jr]['address'][ad]['@addr']
+            if '@name' in nmap_results['nmaprun']['host'][jr]['hostnames']['hostname']:
+                resolved_hostname=nmap_results['nmaprun']['host'][jr]['hostnames']['hostname']['@name']
+                resolve_type=nmap_results['nmaprun']['host'][jr]['hostnames']['hostname']['@type']
+                print(nmap_results['nmaprun']['host'][jr]['address'][ad]['@addrtype'])
+                if '@vendor' in nmap_results['nmaprun']['host'][jr]['address'][ad]:
+                    print(nmap_results['nmaprun']['host'][jr]['address'][ad]['@vendor'])
     
 
 def get_hostname(c):
@@ -146,15 +153,15 @@ def get_ports(c):
 
 length = (len(nmap_results["nmaprun"]["host"]))
 
-
+scan_ops=NmapResaultOperations()
 for x in range(length):
-   # get_address(x)
+    scan_ops.get_address(x)
     #get_hostname(x)
     #get_osdetection(x)
     #get_ports(x)
-    pass
     
-get_scandetails()
+  
+scan_ops.get_scandetails()
 
 
 
---
+++ scanmodule/management/commands/ipaddringest.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand, CommandError
+from scanmodule.models import *
+import pytz
+
+class Command(BaseCommand):
+    help = 'The help information for this command.'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('--host_ip_name', type=str, help='IP address.')
+        parser.add_argument('--resolved_hostname', type=str, help='Hostname')
+        parser.add_argument('--resolve_type', type=str, help='Resolved host type')
+        
+
+        parser.add_argument('--mac_address', type=str, default=None, help='Host MAC address.')
+        parser.add_argument('--mac_addr_type', type=str, default=None, help='Host Mac addr type.')
+
+        parser.add_argument('--mac_vendor', type=str, help='Host MAC Address Vendor.')
+
+        parser.add_argument('--host_state', type=str, help='Host state.')
+        parser.add_argument('--host_state_method', type=str, help='host_state_method.')
+
+        
+        parser.add_argument('--host_state_ttl', type=str, help='host_state_ttl.')        
+
+    def handle(self, *args, **options):
+
+    
+        
+        scan_iphost = HostScanned(
+            host_ip_name=options['host_ip_name'],
+            resolved_hostname=options['resolved_hostname'],
+            resolve_type =options['resolve_type'],
+            mac_address=options['lon'],
+            mac_addr_type=options['lat'],
+            mac_vendor=options['mac_vendor'],
+            host_state=options['host_state'],
+            host_state_method=options['host_state_method'],
+            host_state_ttl=options['host_state_ttl'],
+            
+        )
+        try:
+
+            scan_iphost.save()
+            self.stdout.write(self.style.SUCCESS('Success HostScanned write'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(e))
+
+
+        
